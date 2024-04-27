451de543622b0b4f65b688bf1bf34f6fb5ed224b
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-27 11:56:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,13 @@ from skimage.transform import resize
 
 # Path del modelo preentrenado
 MODEL_PATH = 'resnet50_modelo.h5'
-
-# Dimensiones de las imagenes de entrada    
+    
 width_shape = 224
 height_shape = 224
 
-# Clases
 names = ['ARAÑA ROJA','MOSCA BLANCA','MOSCA FRUTA','PULGON VERDE','PICUDO ROJO']
 
 
-# Se recibe la imagen y el modelo, devuelve la predicción
 def model_prediction(img, model):
 
     img_resize = resize(img, (width_shape, height_shape))
@@ -35,24 +32,22 @@ def main():
     
     model=''
 
-    # Se carga el modelo
+
     if model=='':
         model = load_model(MODEL_PATH)
     
-    st.title("CLASIFICADOR PACIENTES COVID IMAGENES CT")
+    st.title("Clasificador de Aves :sunglasses:")
 
     predictS=""
     img_file_buffer = st.file_uploader("Carga una imagen", type=["png", "jpg", "jpeg"])
     
-    # El usuario carga una imagen
     if img_file_buffer is not None:
         image = np.array(Image.open(img_file_buffer))    
         st.image(image, caption="Imagen", use_column_width=False)
     
-    # El botón predicción se usa para iniciar el procesamiento
     if st.button("Predicción"):
          predictS = model_prediction(image, model)
-         st.success('EL DIAGNÓSTICO ES: {}'.format(names[np.argmax(predictS)]))
+         st.success('LA CLASE ES: {}'.format(names[np.argmax(predictS)]))
 
 
 if __name__ == '__main__':
