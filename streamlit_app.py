237c7fc579b5ba1c62182dc34dd3773adb54e66f
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-04-27 12:28:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,43 +2,33 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 
 import numpy as np
-
-
 import streamlit as st
 from PIL import Image
 from skimage.transform import resize
 
-
 # Path del modelo preentrenado
 MODEL_PATH = 'models_resnet50.h5'
-    
+
 width_shape = 224
 height_shape = 224
 
-names = ['ARAÑA ROJA','MOSCA BLANCA','MOSCA FRUTA','PULGON VERDE','PICUDO ROJO']
-
+names = ['ARAÑA ROJA', 'MOSCA BLANCA', 'MOSCA FRUTA', 'PULGON VERDE', 'PICUDO ROJO']
 
 def model_prediction(img, model):
-
     img_resize = resize(img, (width_shape, height_shape))
-    x=preprocess_input(img_resize*255)
-    x = np.expand_dims(x,axis=0)
+    x = preprocess_input(img_resize * 255)
+    x = np.expand_dims(x, axis=0)
     
     preds = model.predict(x)
     return preds
 
-
 def main():
-    
-    model=''
-
-
-    if model=='':
-        model = load_model(MODEL_PATH)
+    # Cargar el modelo
+    model = load_model(MODEL_PATH)
     
     st.title("Clasificador de Aves :sunglasses:")
 
-    predictS=""
+    predictS = ""
     img_file_buffer = st.file_uploader("Carga una imagen", type=["png", "jpg", "jpeg"])
     
     if img_file_buffer is not None:
@@ -46,9 +36,8 @@ def main():
         st.image(image, caption="Imagen", use_column_width=False)
     
     if st.button("Predicción"):
-         predictS = model_prediction(image, model)
-         st.success('LA CLASE ES: {}'.format(names[np.argmax(predictS)]))
-
+        predictS = model_prediction(image, model)
+        st.success('LA CLASE ES: {}'.format(names[np.argmax(predictS)]))
 
 if __name__ == '__main__':
     main()
